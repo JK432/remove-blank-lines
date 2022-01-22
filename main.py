9fd71f47fcc230cf25cd604b55770140e27d6e5b
Jayakrishnan S <72574346+JK432@users.noreply.github.com>
2022-01-22 17:52:08 +0000

    nothing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Develop a python code to read a text file, copy the contents to another file after removing the blank lines. 
 f1=open("file1.txt","r")
 f2=open("file2.txt","w")
-f2.write("kdfsl")
+
 M=f1.read()
 L=""
 for i in M:
