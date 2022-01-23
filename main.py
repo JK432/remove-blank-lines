d53291df4a437991e02de18823400a4454bc232f
Jayakrishnan S <72574346+JK432@users.noreply.github.com>
2022-01-23 08:18:40 +0000

    error handle

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Develop a python code to read a text file, copy the contents to another file after removing the blank lines. 
-f1=open("file1.txt","r")
-f2=open("file2.txt","w")
+# Develop a python code to read a text file, copy the contents to another file after removing the blank lines.
 
+
+f1=open("file1.txt","r")
 M=f1.read()
 L=""
 for i in M:
   if(i!="\n"):
      L= L+i
 
+f2=open("file2.txt","w+")
 f2.write(L)
+f2.close()
